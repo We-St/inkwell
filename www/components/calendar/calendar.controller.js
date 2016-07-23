5eb213d12fe8326cffe53c74893050753f66e55e
Stefan Weghofer <st.weghofer@gmail.com>
2016-07-23 12:11:18 +0200

    Navigation

--- www/components/calendar/calendar.controller.js
+++ www/components/calendar/calendar.controller.js
@@ -6,7 +6,29 @@ calendarModule.controller('CalendarController', function($scope) {
 	$scope.facts = [
     { title: 'Beers at WerkzeugH' },
     { title: 'Badminton' },
+    { title: 'Sunrise at X' },
+    { title: 'Sunrise at X' },
+    { title: 'Sunrise at X' },
+    { title: 'Sunrise at X' },
+    { title: 'Sunrise at X' },
+    { title: 'Sunrise at X' },
+    { title: 'Sunrise at X' },
+    { title: 'Sunrise at X' },
+    { title: 'Sunrise at X' },
+    { title: 'Sunrise at X' },
+    { title: 'Sunrise at X' },
     { title: 'Sunrise at X' }
   ];
+
+
+  // Filled by the ion-slider directive.
+  $scope.ui = { 
+  	slideOptions: { },
+		slider: null
+	};
+
+  $scope.$on('$ionicSlides.slideChangeEnd', function(event, data) {
+  	console.log('Slide', data.activeIndex, $scope.ui.slider);
+  });
 });
 
